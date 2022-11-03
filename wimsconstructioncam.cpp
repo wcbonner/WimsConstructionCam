36f06b807a3442b2918a2203c9f4ad5414bf7de4
William C Bonner <wbonner@wimsworld.com>
2022-11-02 18:57:15 -0700

    First Sync as Standalone Repository

--- wimsconstructioncam.cpp
+++ wimsconstructioncam.cpp
@@ -41,7 +41,7 @@
 // https://www.ubuntupit.com/best-gps-tools-for-linux/
 // https://www.linuxlinks.com/GPSTools/
 /////////////////////////////////////////////////////////////////////////////
-static const std::string ProgramVersionString("WimsConstructionCam 1.20221026-2 Built " __DATE__ " at " __TIME__);
+static const std::string ProgramVersionString("WimsConstructionCam 1.20221102-1 Built " __DATE__ " at " __TIME__);
 int ConsoleVerbosity = 1;
 int TimeoutMinutes = 0;
 bool UseGPSD = false;
