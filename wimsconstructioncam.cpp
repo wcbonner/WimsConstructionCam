7a697ec7de46694378bbd7ad4118fd77832d204b
William C Bonner <wimbonner@hotmail.com>
2023-03-28 14:54:08 -0700

    again

--- wimsconstructioncam.cpp
+++ wimsconstructioncam.cpp
@@ -65,7 +65,7 @@
 // https://www.ubuntupit.com/best-gps-tools-for-linux/
 // https://www.linuxlinks.com/GPSTools/
 /////////////////////////////////////////////////////////////////////////////
-static const std::string ProgramVersionString("WimsConstructionCam 1.20230328-3 Built " __DATE__ " at " __TIME__);
+static const std::string ProgramVersionString("WimsConstructionCam 1.20230328-4 Built " __DATE__ " at " __TIME__);
 int ConsoleVerbosity(1);
 int TimeoutMinutes(0);
 bool UseGPSD(false);
@@ -679,7 +679,7 @@ std::string GetHostnameFromMediaDirectory(const std::string& MediaDirectory)
 	{
 		HostName.erase(HostName.find("/DCIM"));
 		if (HostName.rfind("/") != std::string::npos)
-			HostName.erase(HostName.front(), HostName.rfind("/"));
+			HostName.erase(0, HostName.rfind("/"));
 	}
 	return(HostName);
 }
